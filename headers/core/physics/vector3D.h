6119404d43694397f628a5a6c6dd00afe13639f1
Sebastian Einarsson <sebastian.einarsson03@gmail.com>
2026-02-16 11:38:09 +0100

    added missing headers

--- headers/core/physics/vector3D.h
+++ headers/core/physics/vector3D.h
@@ -1,6 +1,9 @@
 #pragma once
-#include <cmath>
 #include "DirectXMath.h"
+
+#include <cmath>
+#include <string>
+
 class Vector3D {
 private:
 	float x;
@@ -8,7 +11,7 @@ private:
 	float z;
 
 public:
-	Vector3D(float xValue, float yValue, float zValue); 
+	Vector3D(float xValue, float yValue, float zValue);
 
 	~Vector3D() = default;
 	Vector3D(const Vector3D& other) = default;
@@ -18,13 +21,12 @@ public:
 	Vector3D(Vector3D&& other) = default;
 	Vector3D& operator=(Vector3D&& other) = default;
 
-	Vector3D operator+(const Vector3D& right) const; 
-	Vector3D operator-(const Vector3D& right) const; 
+	Vector3D operator+(const Vector3D& right) const;
+	Vector3D operator-(const Vector3D& right) const;
 	float operator*(const Vector3D& right) const;	 // Dot product
 	Vector3D operator^(const Vector3D& right) const; // Cross product
-	Vector3D operator*(float scalar) const;		 // Scalar mult
+	Vector3D operator*(float scalar) const;			 // Scalar mult
 
-	
 	float GetX() const;
 	float GetY() const;
 	float GetZ() const;
@@ -32,11 +34,11 @@ public:
 	/// returns the lenght of the vector
 	/// </summary>
 	/// <returns></returns>
-	float Length() const; 
+	float Length() const;
 	/// <summary>
 	/// normalizes the vector to a lenght of 1
 	/// </summary>
-	void Normalize();	  
+	void Normalize();
 
 	std::string GetString();
 };

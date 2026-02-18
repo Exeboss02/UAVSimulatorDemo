13e01ad664cabe488130db52f1315616d123a921
Sebastian Einarsson <sebastian.einarsson03@gmail.com>
2026-02-18 15:08:08 +0100

    surely

--- headers/gameObjects/mesh.h
+++ headers/gameObjects/mesh.h
@@ -4,6 +4,7 @@
 #include "rendering/texture.h"
 #include "rendering/vertex.h"
 #include "rendering/vertexBuffer.h"
+#include <DirectXCollision.h>
 #include <array>
 #include <string>
 #include <vector>
@@ -28,21 +29,26 @@ private:
 class Mesh {
 public:
 	Mesh() = default;
-	Mesh(VertexBuffer vertexbuffer, IndexBuffer indexbuffer, std::vector<SubMesh>&& submeshes);
+	Mesh(VertexBuffer vertexbuffer, IndexBuffer indexbuffer, std::vector<SubMesh>&& submeshes,
+		 DirectX::BoundingBox boundingBox);
 	Mesh(Mesh&&) noexcept = default;
 	Mesh& operator=(Mesh&&) noexcept = default;
 	~Mesh();
 
+	void Init(VertexBuffer vertexbuffer, IndexBuffer indexbuffer, std::vector<SubMesh>&& submeshes,
+			  DirectX::BoundingBox boundingBox);
 	void Init(VertexBuffer vertexbuffer, IndexBuffer indexbuffer, std::vector<SubMesh>&& submeshes);
 
 	std::vector<SubMesh>& GetSubMeshes();
 	VertexBuffer& GetVertexBuffer();
 	IndexBuffer& GetIndexBuffer();
-
 	std::string& GetIdentifier();
+	const DirectX::BoundingBox& GetBoundingBox() const;
+
 	void SetIdentifier(std::string identifier);
 
 private:
+	DirectX::BoundingBox boundingBox;
 	std::string identifier;
 	std::vector<SubMesh> subMeshes;
 	VertexBuffer vertexbuffer;
--- src/gameObjects/mesh.cpp
+++ src/gameObjects/mesh.cpp
@@ -1,58 +1,39 @@
 #include "gameObjects/mesh.h"
 
-Mesh::Mesh(VertexBuffer vertexbuffer, IndexBuffer indexbuffer, std::vector<SubMesh>&& submeshes) 
-    : vertexbuffer(std::move(vertexbuffer)), indexbuffer(std::move(indexbuffer)), subMeshes(std::move(submeshes)) {
-}
+Mesh::Mesh(VertexBuffer vertexbuffer, IndexBuffer indexbuffer, std::vector<SubMesh>&& submeshes,
+		   DirectX::BoundingBox boundingBox)
+	: vertexbuffer(std::move(vertexbuffer)), indexbuffer(std::move(indexbuffer)), subMeshes(std::move(submeshes)),
+	  boundingBox(boundingBox) {}
 
-Mesh::~Mesh() {
-
-}
-
-std::string& Mesh::GetIdentifier()
-{
-    return this->identifier;
-}
-
-std::vector<SubMesh>& Mesh::GetSubMeshes()
-{
-    return this->subMeshes;
-}
-
-VertexBuffer& Mesh::GetVertexBuffer()
-{
-    return this->vertexbuffer;
-}
-
-IndexBuffer& Mesh::GetIndexBuffer()
-{
-    return this->indexbuffer;
-}
-
-void Mesh::SetIdentifier(std::string name)
-{
-    this->identifier = name;
-}
-
-SubMesh::SubMesh(size_t startIndex, size_t nrOfIndices)
-    : startIndex(startIndex), nrOfIndices(nrOfIndices) {
-
-}
-SubMesh::~SubMesh() {
-
-}
-
-size_t SubMesh::GetStartIndex() const
-{
-    return this->startIndex;
-}
-
-size_t SubMesh::GetNrOfIndices() const
-{
-    return this->nrOfIndices;
-}
+Mesh::~Mesh() {}
 
 void Mesh::Init(VertexBuffer vertexbuffer, IndexBuffer indexbuffer, std::vector<SubMesh>&& submeshes) {
-    this->vertexbuffer = std::move(vertexbuffer);
+	this->Init(std::move(vertexbuffer), std::move(indexbuffer), std::move(submeshes), DirectX::BoundingBox{});
+}
+
+std::string& Mesh::GetIdentifier() { return this->identifier; }
+
+std::vector<SubMesh>& Mesh::GetSubMeshes() { return this->subMeshes; }
+
+VertexBuffer& Mesh::GetVertexBuffer() { return this->vertexbuffer; }
+
+IndexBuffer& Mesh::GetIndexBuffer() { return this->indexbuffer; }
+
+void Mesh::SetIdentifier(std::string name) { this->identifier = name; }
+
+const DirectX::BoundingBox& Mesh::GetBoundingBox() const { return this->boundingBox; }
+
+SubMesh::SubMesh(size_t startIndex, size_t nrOfIndices) : startIndex(startIndex), nrOfIndices(nrOfIndices) {}
+SubMesh::~SubMesh() {}
+
+size_t SubMesh::GetStartIndex() const { return this->startIndex; }
+
+size_t SubMesh::GetNrOfIndices() const { return this->nrOfIndices; }
+
+void Mesh::Init(VertexBuffer vertexbuffer, IndexBuffer indexbuffer, std::vector<SubMesh>&& submeshes,
+				DirectX::BoundingBox boundingBox) {
+	this->vertexbuffer = std::move(vertexbuffer);
 	this->indexbuffer = std::move(indexbuffer);
 	this->subMeshes = std::move(submeshes);
+	this->boundingBox = boundingBox;
 }

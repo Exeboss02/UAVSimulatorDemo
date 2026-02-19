14e667587682ffca03b743e8d84050714050fbcb
DagOhlin <ohlin.dag@gmail.com>
2026-02-19 11:01:10 +0000

    player is now c++ created and is smaller

--- src/game/player.cpp
+++ src/game/player.cpp
@@ -1,16 +1,55 @@
 #include "game/player.h"
+#include "core/physics/sphereCollider.h"
 
 void Player::Start()
 {
 	this->RigidBody::Start();
-
+	 //adding camera
 	auto cameraWeak = this->factory->CreateGameObjectOfType<CameraObject>();
 	auto cameraShared = cameraWeak.lock();
-	DirectX::XMFLOAT3 pos(0.0f, 4.0f, 0.0f);
+	DirectX::XMFLOAT3 pos(0.0f, 2.0f, 0.0f);
 	cameraShared->transform.SetPosition(DirectX::XMLoadFloat3(&pos));
 	cameraShared->SetParent(this->GetPtr());
 
 	this->camera = cameraShared;
+
+	//adding body colliders
+	{
+	auto colliderobjWeak = this->factory->CreateGameObjectOfType<SphereCollider>();
+	auto colliderobj = colliderobjWeak.lock();
+	colliderobj->dynamic = true;
+	colliderobj->solid = true;
+	DirectX::XMFLOAT3 pos(0.0f, 0.0f, 0.0f);
+	colliderobj->transform.SetPosition(DirectX::XMLoadFloat3(&pos));
+	DirectX::XMFLOAT3 scale(1.0f, 1.0f, 1.0f);
+	colliderobj->transform.SetScale(DirectX::XMLoadFloat3(&scale));
+	colliderobj->SetParent(this->GetPtr());
+	}
+
+	{
+		auto colliderobjWeak = this->factory->CreateGameObjectOfType<SphereCollider>();
+		auto colliderobj = colliderobjWeak.lock();
+		colliderobj->dynamic = true;
+		colliderobj->solid = true;
+		DirectX::XMFLOAT3 pos(0.0f, 0.5f, 0.0f);
+		colliderobj->transform.SetPosition(DirectX::XMLoadFloat3(&pos));
+		DirectX::XMFLOAT3 scale(1.0f, 1.0f, 1.0f);
+		colliderobj->transform.SetScale(DirectX::XMLoadFloat3(&scale));
+		colliderobj->SetParent(this->GetPtr());
+	}
+
+	{
+		auto colliderobjWeak = this->factory->CreateGameObjectOfType<SphereCollider>();
+		auto colliderobj = colliderobjWeak.lock();
+		colliderobj->dynamic = true;
+		colliderobj->solid = true;
+		DirectX::XMFLOAT3 pos(0.0f, 1.0f, 0.0f);
+		colliderobj->transform.SetPosition(DirectX::XMLoadFloat3(&pos));
+		DirectX::XMFLOAT3 scale(1.0f, 1.0f, 1.0f);
+		colliderobj->transform.SetScale(DirectX::XMLoadFloat3(&scale));
+		colliderobj->SetParent(this->GetPtr());
+	}
+
 }
 
 void Player::Tick()
@@ -20,6 +59,7 @@ void Player::Tick()
 	this->input[0] = this->keyBoardInput.GetMovementVector().data()[0];
 	this->input[1] = this->keyBoardInput.GetMovementVector().data()[1];
 	this->UpdateCamera();
+	this->shootRay();
 
 	//Logger::Log("linear velocity: " + std::to_string(this->linearVelocity.x) + ", " + std::to_string(this->linearVelocity.y) + ", " + std::to_string(this->linearVelocity.z));
 }
@@ -109,4 +149,58 @@ void Player::SaveToJson(nlohmann::json& data)
 
 	data["speed"] = this->speed;
 	data["mouseSensitivity"] = this->mouseSensitivity;
+}
+
+void Player::shootRay() {
+
+	const DirectX::XMVECTOR lookVec = this->camera.lock()->GetGlobalForward();
+	const DirectX::XMVECTOR posVec = this->camera.lock()->GetGlobalPosition();
+	//{
+	//	Ray ray{Vector3D{posVec}, Vector3D{lookVec}};
+	//	RayCastData rayCastData;
+	//	//Logger::Log("shooting ray");
+	//	// Logger::Log("shooting ray from pos: ", ray.origin.GetX(), " ", ray.origin.GetY(), " ", ray.origin.GetZ());
+	//	// Logger::Log("with direction: ", ray.direction.GetX(), " ", ray.direction.GetY(), " ", ray.direction.GetZ());
+
+	//	bool didHit = PhysicsQueue::GetInstance().castRay(ray, rayCastData);
+	//	std::string hitString;
+	//	if (didHit) {
+
+	//		std::shared_ptr<GameObject> hitCollider = rayCastData.hitColider.lock();
+	//		std::weak_ptr<GameObject> weakParent = hitCollider->GetParent();
+	//		if (!weakParent.expired()) {
+	//			std::shared_ptr<GameObject> strongParent = weakParent.lock();
+	//			strongParent->OnObserve();
+	//		}
+	//	} else {
+	//		hitString = "miss";
+	//	}
+
+	//	//Logger::Log(hitString, " at distance: ", std::to_string(rayCastData.distance));
+	//}
+	if (this->keyBoardInput.LeftClick()) {
+
+		Ray ray{Vector3D{posVec}, Vector3D{lookVec}};
+		RayCastData rayCastData;
+		Logger::Log("shooting ray");
+		// Logger::Log("shooting ray from pos: ", ray.origin.GetX(), " ", ray.origin.GetY(), " ", ray.origin.GetZ());
+		// Logger::Log("with direction: ", ray.direction.GetX(), " ", ray.direction.GetY(), " ", ray.direction.GetZ());
+
+		bool didHit = PhysicsQueue::GetInstance().castRay(ray, rayCastData);
+		std::string hitString;
+		if (didHit) {
+
+			std::shared_ptr<GameObject> hitCollider = rayCastData.hitColider.lock();
+			std::weak_ptr<GameObject> weakParent = hitCollider->GetParent();
+			if (!weakParent.expired()) {
+				std::shared_ptr<GameObject> strongParent = weakParent.lock();
+				strongParent->OnInteract();
+			}
+			hitString = "hit";
+		} else {
+			hitString = "miss";
+		}
+
+		Logger::Log(hitString, " at distance: ", std::to_string(rayCastData.distance));
+	}
 }

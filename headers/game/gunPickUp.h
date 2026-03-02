1d1e096e1ef4bf9452d543b7d7dc788d5c867b69
DagOhlin <ohlin.dag@gmail.com>
2026-03-02 11:10:24 +0000

    gunPickUp now works

--- headers/game/gunPickUp.h
+++ headers/game/gunPickUp.h
@@ -18,8 +18,16 @@ public:
 
 
 private:
+	std::string gunVisualPath = "guns/GunTest.glb:Mesh_0";
+	DirectX::XMFLOAT3 visualPosition = DirectX::XMFLOAT3(0, 0, 0);
+	DirectX::XMFLOAT3 visualScale = DirectX::XMFLOAT3(0.007f, 0.007f, 0.007f);
+	DirectX::XMFLOAT3 visualRotationRPY = DirectX::XMFLOAT3(0, 0, 0);
 
-		std::weak_ptr<BoxCollider> interactable;
+	std::weak_ptr<BoxCollider> interactable;
+
+	std::weak_ptr<MeshObject> gunVisual;
+
+	float hoverDisabledUntil = 0.0f;
 
 
 
--- src/game/gunPickUp.cpp
+++ src/game/gunPickUp.cpp
@@ -1,6 +1,5 @@
 #include "game/gunPickUp.h"
-
-
+#include "UI/interactionPrompt.h"
 GunPickUp::GunPickUp() {}
 
 GunPickUp::~GunPickUp() {}
@@ -8,37 +7,65 @@ GunPickUp::~GunPickUp() {}
 void GunPickUp::Start() {
 
 	{
-		MeshObjData meshdata = AssetManager::GetInstance().GetMeshObjData("guns/GunTest.glb:Mesh_0");
-		this->SetMesh(meshdata);
+		auto meshobjweak = this->factory->CreateGameObjectOfType<MeshObject>();
+
+		auto meshobj = meshobjweak.lock();
+
+		meshobj->SetParent(this->GetPtr());
+
+		MeshObjData meshdata = AssetManager::GetInstance().GetMeshObjData(this->gunVisualPath);
+		meshobj->SetMesh(meshdata);
+		meshobj->transform.SetScale(DirectX::XMLoadFloat3(&this->visualScale));
+		meshobj->transform.SetRotationRPY(this->visualRotationRPY.x, this->visualRotationRPY.y,
+										  this->visualRotationRPY.z);
+		meshobj->SetCastShadow(false);
+		this->gunVisual = meshobj;
 	}
 	
 	{
 		auto colliderobj = this->factory->CreateStaticGameObject<BoxCollider>();
 
-		/*DirectX::XMFLOAT3 pos(0.0f, 3.0f, 4.5f);
-		colliderobj->transform.SetPosition(DirectX::XMLoadFloat3(&pos));*/
-		/*DirectX::XMFLOAT3 scale(0.750f, 0.750f, 0.250f);
-		colliderobj->transform.SetScale(DirectX::XMLoadFloat3(&scale));*/
+		DirectX::XMFLOAT3 pos(0.1f, 0.0f, 0.0f);
+		colliderobj->transform.SetPosition(DirectX::XMLoadFloat3(&pos));
+		DirectX::XMFLOAT3 scale(0.9f, 0.3f, 0.1f);
+		colliderobj->transform.SetScale(DirectX::XMLoadFloat3(&scale));
 		colliderobj->SetParent(this->GetPtr());
 		colliderobj->SetOnInteract([&](std::shared_ptr<Player> playerShared) { this->OnInteract(playerShared); });
 		colliderobj->SetOnHover([&] { this->Hover(); });
 		colliderobj->SetTag(Tag::INTERACTABLE);
+		//colliderobj->ShowDebug(true);
 		//colliderobj->SetName("Interactable " + std::to_string(this->factory->GetNextID()));
 
 		this->interactable = colliderobj;
 	}
-
+	//this->interactable.lock()->ShowDebug(true);
 }
 
 void GunPickUp::OnInteract(std::shared_ptr<Player> playerShared) {
-	
-	
-	Logger::Log("give me gun!");
 
+
+	if (playerShared->resources.titanium.DecrementAmount(10)) {
+		playerShared->addGun(Player::Guns::rifle);
+	}
 
 }
 
 void GunPickUp::Hover() {
+	try {
+		float currentTime = Time::GetInstance().GetSessionTime();
+		if (currentTime < this->hoverDisabledUntil) return;
+		auto promptWeak = this->factory->FindObjectOfType<UI::InteractionPrompt>();
+		if (promptWeak.expired()) return;
+		auto prompt = promptWeak.lock();
+		if (!prompt) return;
 
+		std::string txt = "10 titanium to repair gun";
+		DirectX::XMVECTOR worldPos = this->transform.GetGlobalPosition();
+		prompt->Show(txt, worldPos);
+	} catch (const std::exception& e) {
+		Logger::Error("GunPickUp::Hover exception: ", e.what());
+	} catch (...) {
+		Logger::Error("GunPickUp::Hover unknown exception");
+	}
 
 }

5c0be0c805ca932e497345f76164a454b863f62a
Sebastian Einarsson <sebastian.einarsson03@gmail.com>
2026-03-02 14:51:32 +0100

    added hit indicator

--- src/game/crosshair.cpp
+++ src/game/crosshair.cpp
@@ -1,5 +1,6 @@
 #include "game/crosshair.h"
 #include "UI/canvas.h"
+#include "core/tools.h"
 #include "gameObjects/cameraObject.h"
 #include "rendering/renderQueue.h"
 
@@ -9,10 +10,42 @@
 void Crosshair::Start() {
 	// Keep default behavior
 	this->SetAnchor(UI::Anchor::MidCenter);
+	if (this->GetImage().empty()) {
+		this->SetImage(this->defaultImagePath);
+	} else {
+		this->defaultImagePath = this->GetImage();
+	}
+	this->defaultTint = this->GetTint();
 	this->UI::Image::Start();
 }
 
-void Crosshair::Update(float dt) {}
+void Crosshair::Update(float dt) {
+	float effectiveDt = dt;
+	if (effectiveDt <= 0.0f) {
+		effectiveDt = Time::GetInstance().GetDeltaTime();
+	}
+
+	if (this->hitIndicatorTimer > 0.0f) {
+		this->hitIndicatorTimer -= effectiveDt;
+		if (this->hitIndicatorTimer <= 0.0f) {
+			this->SetImage(this->defaultImagePath);
+			this->SetTint(this->defaultTint);
+		}
+	}
+
+	this->UI::Image::Update(effectiveDt);
+}
+
+void Crosshair::ShowHitIndicator(float durationSeconds) {
+	if (!this->GetImage().empty() && this->GetImage() != this->hitImagePath) {
+		this->defaultImagePath = this->GetImage();
+		this->defaultTint = this->GetTint();
+	}
+
+	this->SetImage(this->hitImagePath);
+	this->SetTint(this->hitTint);
+	this->hitIndicatorTimer = durationSeconds;
+}
 
 void Crosshair::ShowInHierarchy() {
 	this->UI::Image::ShowInHierarchy();

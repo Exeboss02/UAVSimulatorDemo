915444f80ec3aecb9fd8beb4fc4439d1655660f9
OuterFrog <91077751+OuterFrog@users.noreply.github.com>
2026-03-09 15:14:22 +0100

    Eye and side of generator is now "lit"

--- src/game/resourceGenerator.cpp
+++ src/game/resourceGenerator.cpp
@@ -17,6 +17,7 @@
 
 void ResourceGenerator::Start() {
 	this->SetMesh(AssetManager::GetInstance().GetMeshObjData("meshes/generator.glb:Mesh_0"));
+	this->GetMesh().SetMaterial(2, AssetManager::GetInstance().GetMaterialWeakPtr("newGeneratorSide").lock());
 	//this->transform.SetScale(0.5f, 1.2f, 0.5f);
 	//this->transform.SetPosition(0, 1.8f, 0);
 
